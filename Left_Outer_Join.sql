d97582f0da98eb35aa332ce0fa4be69ac4978012
SANA SABIR <93030358+sanasabir@users.noreply.github.com>
2024-06-23 17:23:47 +0530

    Update Left_Outer_Join.sql

--- Left_Outer_Join.sql
+++ Left_Outer_Join.sql
@@ -1,4 +1,6 @@
-CREATE TABLE customers (
+-- We've created a database for customers and their orders. Not all of the customers have made orders, however. Come up with a query that lists the name and email of every customer followed by the item and price of orders they've made. Use a LEFT OUTER JOIN so that a customer is listed even if they've made no orders, and don't add any ORDER BY.
+    
+    CREATE TABLE customers (
     id INTEGER PRIMARY KEY AUTOINCREMENT,
     name TEXT,
     email TEXT);
